dcd5063b145194c27b58ec824d502f344d7671ea
Amarnath TSR <tsr.amarnath@gmail.com>
2020-06-28 19:05:41 +0530

    config files updated

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,3 +2,4 @@ import dotenv from 'dotenv';
 dotenv.config();
 export const mongoURI : string = process.env.MONGO_URI ? process.env.MONGO_URI : '';
 export const port : string = process.env.PORT ? process.env.PORT : '';
+export const secretOrKey : string = process.env.SECRET_OR_KEY ? process.env.SECRET_OR_KEY : "";
