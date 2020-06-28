d653e751de76bc2715e3f973c527789f3a1adfa5
Amarnath TSR <tsr.amarnath@gmail.com>
2020-06-28 06:12:06 +0530

    mongoose installed and connection established

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
-import express, { Application, Request, Response, NextFunction  } from 'express';
-
-const app : Application = express();
-
-app.get('/', function (req : Request, res: Response, next : NextFunction) {
-    res.send('Hello World!');
+import express, { Application, Request, Response, NextFunction } from "express";
+import mongoose, { connect } from "mongoose";
+import { mongoURI } from "./utils/config";
+const app: Application = express();
+mongoose
+  .connect(mongoURI, { useUnifiedTopology: true, useNewUrlParser: true  })
+  .then((connectStatus) => {
+    console.log("database connections established");
+  })
+  .catch((error) => {
+    console.log(error);
   });
-  
-  app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-  });
+app.get("/", function (req: Request, res: Response, next: NextFunction) {
+  res.send("Hello World!");
+});
+
+app.listen(3000, function () {
+  console.log("Example app listening on port 3000!");
+});
