d6a0fc81e11c9a91d8219d5b1848fe4e8da00dcc
Amarnath TSR <tsr.amarnath@gmail.com>
2020-06-28 10:46:44 +0530

    add port confiq and proper response and comments

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express, { Application, Request, Response, NextFunction } from "express";
 import mongoose, { connect } from "mongoose";
 import { mongoURI } from "./utils/config";
+import {port} from './utils/config';
 const app: Application = express();
+/* Establised mongodb connections*/
 mongoose
   .connect(mongoURI, { useUnifiedTopology: true, useNewUrlParser: true  })
   .then((connectStatus) => {
@@ -10,10 +12,12 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+
+/* base route test response */  
 app.get("/", function (req: Request, res: Response, next: NextFunction) {
-  res.send("Hello World!");
+ return res.json({success:true, message:'api working fine', data:{}});
 });
 
-app.listen(3000, function () {
-  console.log("Example app listening on port 3000!");
+app.listen(port, function () {
+  console.log(`Example app listening on port http://localhost:${port}`);
 });
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,3 +1,4 @@
 import dotenv from 'dotenv';
 dotenv.config();
 export const mongoURI : string = process.env.MONGO_URI ? process.env.MONGO_URI : '';
+export const port : string = process.env.PORT ? process.env.PORT : '';
